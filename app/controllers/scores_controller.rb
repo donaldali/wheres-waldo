3bed897d93a0d485b8425f10b03b5c26a3136b14
Donald Ali <donaldali@yahoo.com>
2014-10-03 15:57:08 -0700

    Make scores visible to only their owner
    Create a before_action to check that the player that wants to view a
    score is the player that got that score.

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,6 +1,7 @@
 class ScoresController < ApplicationController
 
   before_action :some_player
+  before_action :correct_player, only: :show
 
   def show
     number_of_top_scores = 20
@@ -8,5 +9,13 @@ class ScoresController < ApplicationController
     @score = Score.find(params[:id])
     @top_scores = @score.board.top_scores( number_of_top_scores )
     @made_leaderboard = @top_scores.map(&:id).include?(@score.id)
+  end
+
+  private
+
+  def correct_player
+    unless get_player.id == Score.find(params[:id]).player.id
+      redirect_to boards_url, notice: "You can only view your score"
+    end
   end
 end
